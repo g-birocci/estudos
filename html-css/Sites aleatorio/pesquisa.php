7986eb33f4954ac5ce88b34430f02f7899acfcfa
g-birocci <gabrielbirocci@hotmail.com>
2024-07-01 17:14:04 -0100

    update

---
+++ html-css/Sites aleatorio/pesquisa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Gestão de Clientes</title>
+    </head>
+    <body>
+    <h1>Gestão de clientes</h1>
+    <br/><br/>
+    <div class="linha">
+        <div class="celula"></div>
+        <div class="celula">
+        <a href="listar.php" target="_self">Listar</a><br/>
+        <a href="inserir.html" target="_self">Inserir</a><br/>
+        <a href="eliminar1.php" target="_self">Eliminar</a><br/>
+        <a href="alterar1.php" target="_self">Alterar</a><br/>
+        <a href="pesquisa.html" target="_self">pesquisa</a>
+    </div>
+    </div>
+    </body>
+        
+</html>
