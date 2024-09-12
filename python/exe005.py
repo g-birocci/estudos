93b825dd5726e8969fcee4d82b3711786313cc03
g-birocci <gabrielbirocci@hotmail.com>
2024-09-12 09:46:17 +0100

    estudos

--- python/exe005.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Digite o priemro numero"))
-num2 = int(input("Digite o segundo numero"))
-
-resul = num1 + num2
-print(resul)
---
+++ python/exe006.py
@@ -0,0 +1,12 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+divisao = num1 / num2
+multi = num1 * num2 
+
+print("A soma dos numeros é " + str(soma))
+print("A subtração dos numeros é " + str(subtracao))
+print("A divisão dos numeros é " + str(divisao))
+print("A multiplicação dos numeros é " + str(multi))
