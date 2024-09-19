d905ceee2a82fdae2b3a0e4d1a5f931968fce845
g-birocci <gabrielbirocci@hotmail.com>
2024-09-19 19:41:25 +0100

    update

--- python/exe016.py
+++ python/exe016.py
@@ -1,19 +1,18 @@
-print("Digite os ingredientes na lista !!!")
-lis_ta = []
-for i in range(10):
-    sair = False
-    while not sair:
-        itens = input("Digite os itens: ").strip().lower()
-        remo = input("Deseja continuar ou remover itens [Sim/Remover]")
-        if remo == "remover":
-            Remo = input("Qual itens deseja removover? ")
-            lis_ta.remove(remo)
-            print(lis_ta)
-        else:
-            if itens == "sair":
-                sair = True
-                print("Lista final de ingredientes:", lis_ta)
-                break
-            else:
-                lis_ta.append(itens)
-
+print("Insira 7 numeros entre PAR e IMPAR.  ")
+numeros = []
+par = []
+impar = []
+for i in range(7):
+    valores = int(input("Quais numeros?  "))
+    numeros.append(valores)   
+    if valores % 2 == 0:
+        par.append(valores)      
+    else:
+        impar.append(valores)       
+if len(par) > len(impar):
+    print("mais Par!")
+else:
+    print("Mais Impar!")   
+print(numeros)
+print(par)
+print(impar)
---
+++ python/exe017.py
@@ -0,0 +1,29 @@
+print("Digite os ingredientes na lista !!!")
+lista = []
+is_valid = False
+while is_valid == False:
+    if len(lista) >= 10:
+            print("Só pode ter 10 itens na lista!")
+            is_valid = True
+            break
+    itens = input("Digite um item para a lista: ")
+    if itens.lower() == "sair":
+        break
+    lista.append(itens)
+    continuar = input("Deseja continuar ? ou Remover digite [Remover]")
+    if continuar.lower() == "sim":
+        continue    
+    elif continuar.lower() == "remover":
+        if lista:
+            item_to_remove = input("Digite o item que deseja remover: ")
+            if item_to_remove in lista:
+                lista.remove(item_to_remove)
+            else:
+                print("O item não está na lista.")
+        else:
+            print("A lista está vazia, nada para remover.")    
+    else:
+        print("Input errado, programa encerrado.")
+        break
+print(lista)
+print(len(lista))
