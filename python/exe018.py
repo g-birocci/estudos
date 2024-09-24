1ca70b936b4e53dc4527f71b09828137dcd49a3d
g-birocci <gabrielbirocci@hotmail.com>
2024-09-24 15:32:10 +0100

    update

---
+++ python/exe018.py
@@ -0,0 +1,14 @@
+nome = {}
+idade = {}
+notas = {}
+cidade = {}
+
+ask =input("Digita [Sim] para cadastrar um aluno ou [remover] pra excluir ?")
+if ask.lower() == "sim":
+    nome = input("Digite o nome do aluno: ")
+    idade = input("Digite a idade: ")
+    notas = input("Digite a nota: ")
+    cidade = input("Digite a cidade: ")
+print("CADASTRO FEITO!")
+print("nome")
+
