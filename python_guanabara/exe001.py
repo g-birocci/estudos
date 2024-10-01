607ce1fa8a0cb7bd69e8e1f711611169182fdf05
g-birocci <gabrielbirocci@hotmail.com>
2024-10-01 16:18:17 +0100

    update

---
+++ python_guanabara/exe001.py
@@ -0,0 +1,3 @@
+nome = input("Qual o seu nome ? ")
+peso = input("Qual o seu peso ? ")
+idadde = input("QUal a sua idade ? ")
---
+++ python_guanabara/exe003.py
@@ -0,0 +1,4 @@
+dia = int(input("Qual o dia que você nasceu? "))
+mes = int(input("Qual o mês que você nasceu? "))
+ano = int(input("Em que ano você nasceu? "))
+print("Você nasceu no dia " + str(dia) + " de " + str(mes) + " de " + str(ano) + ". Correto?")
