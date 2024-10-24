7e2208418ea2659cc975c8ebe6a98f3af2007c54
g-birocci <gabrielbirocci@hotmail.com>
2024-10-24 19:39:03 +0100

    estudos

--- python/exe23.py
+++ python/exe23.py
@@ -1,5 +1,20 @@
-liista = {}
-print("Bem Vindo")
-nome = input("Digite o seu nome: ")
-tel = int(input("Digite o telefone: "))
-email = input("Digite o seu email: ")
+liista = []
+contacto={}
+
+while True:
+   
+    print("Bem Vindo")
+    nome = input("Digite o seu nome: ")
+    telefone = int(input("Digite o telefone: "))
+    email = 
+    n_tags = int(input("quantas tags?"))
+    tags = []
+    for n in range(0,n_tags):
+        tag = input("Insira a tag: amigo, trabalho ou familia")
+        tags.append(tag)
+    set_tags = set(tags)
+
+    contacto.update({"nome": nome, "telefone": telefone, "email": email, "tags": set_tags})
+    liista.append(contacto)
+
+    print(liista)
